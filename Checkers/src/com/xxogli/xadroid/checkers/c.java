77d6f119c87a1d3ab579e321ad3d1f27142255d3
wfrancois45 <wfrancois45@gmail.com>
2014-01-15 00:06:58 +0100

    Suppression des erreurs de a, c, f et g. Pour les 4 derniers, je pense qu'il faut faire quelque chose de la CheckersView du constructeur et this.a doit l'être.

--- Checkers/src/com/xxogli/xadroid/checkers/c.java
+++ Checkers/src/com/xxogli/xadroid/checkers/c.java
@@ -3,21 +3,18 @@ package com.xxogli.xadroid.checkers;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 
-class c
-  implements DialogInterface.OnClickListener
-{
-  c(ChekersView paramb)
-  {
-  }
+class c implements OnClickListener {
+	c(ChekersView paramb) {
+	}
 
-  public void onClick(DialogInterface paramDialogInterface, int paramInt)
-  {
-    if (ChekersView.a(this.a, false, -1, 0, 0, 0))
-      this.a.postInvalidate();
-  }
+	public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+		// if (ChekersView.a(this.a, false, -1, 0, 0, 0))
+		// this.a.postInvalidate();
+	}
 }
 
-/* Location:           /home/willy/git/projet-secu-applis-jerawizo/com.xxogli.xadroid.checkers_dex2jar.jar
- * Qualified Name:     com.xxogli.xadroid.checkers.c
- * JD-Core Version:    0.6.2
+/*
+ * Location:
+ * /home/willy/git/projet-secu-applis-jerawizo/com.xxogli.xadroid.checkers_dex2jar
+ * .jar Qualified Name: com.xxogli.xadroid.checkers.c JD-Core Version: 0.6.2
  */
--- Checkers/src/com/xxogli/xadroid/checkers/f.java
+++ Checkers/src/com/xxogli/xadroid/checkers/f.java
@@ -3,20 +3,17 @@ package com.xxogli.xadroid.checkers;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 
-class f
-  implements DialogInterface.OnClickListener
-{
-  f(ChekersView paramb)
-  {
-  }
+class f implements OnClickListener {
+	f(ChekersView paramb) {
+	}
 
-  public void onClick(DialogInterface paramDialogInterface, int paramInt)
-  {
-    this.a.e(true);
-  }
+	public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+		// this.a.e(true);
+	}
 }
 
-/* Location:           /home/willy/git/projet-secu-applis-jerawizo/com.xxogli.xadroid.checkers_dex2jar.jar
- * Qualified Name:     com.xxogli.xadroid.checkers.f
- * JD-Core Version:    0.6.2
+/*
+ * Location:
+ * /home/willy/git/projet-secu-applis-jerawizo/com.xxogli.xadroid.checkers_dex2jar
+ * .jar Qualified Name: com.xxogli.xadroid.checkers.f JD-Core Version: 0.6.2
  */
